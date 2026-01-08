a248ebc55a17c9a276c6782e71cb7711f747e849
MazumalaAja <programlia1108@gmail.com>
2026-01-09 06:58:11 +0700

    continue module-1.py

--- modules/module-1.py
+++ modules/module-1.py
@@ -1,1 +1,23 @@
+# Membuat program hello world...
+print("===== start Program 1 start =====")
+
 print("Hello World")
+
+print("===== end Program 1 end =====")
+
+# Variable dan tipe data..
+print("===== start Program 2 start =====")
+
+nama = "Zumal" # tipe data text / string
+tinggi = 169.5 # tipe data float
+berat_badan = 58 # tipe data integer
+menikah = False # tipe data boolean
+
+# Menampilkan output
+print(f"Hallo saya : {nama} \n Tinggi saya : {tinggi} \n Berat badan saya {berat_badan} \n Apakah saya udah menikah ? {menikah}")
+
+# Mengecek tipe data
+print(type(tinggi))
+print("===== end Program 2 end =====")
+
+
